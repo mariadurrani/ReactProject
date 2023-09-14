be5adc1edf634ac573f5cd38317bd615cf91c2f6
Maria Durrani <maria_durrani@hotmail.com>
2023-09-14 18:55:31 +0200

    Navbar file

--- projectreact/src/App.js
+++ projectreact/src/App.js
@@ -1,8 +1,13 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import Navbar from "./components/navbar";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ projectreact/src/components/navbar.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar"></nav>
+    </>
+  );
+}
+
+export default Navbar;
