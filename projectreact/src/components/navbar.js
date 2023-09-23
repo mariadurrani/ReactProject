6f318e8aabfb94dfe70eed6e8ffbe330e7632453
Maria Durrani <maria_durrani@hotmail.com>
2023-09-23 11:49:07 -0600

    icons

--- projectreact/src/components/navbar.js
+++ projectreact/src/components/navbar.js
@@ -1,10 +1,26 @@
-import { useState } from "react";
+import "./navbar.css";
+import React from "react";
+import { MenuItems } from "./Menuitems";
 
 function Navbar() {
   return (
-    <>
-      <nav className="navbar"></nav>
-    </>
+    <nav className="NavbarItems">
+      <h1 className="navbar-logo">TRAVL</h1>
+      <ul className="nav-menu">
+        {MenuItems.map((item, index) => {
+          return (
+            <li>
+              {
+                <a href="/">
+                  <i className={item.icon}></i>
+                  {item.title}
+                </a>
+              }
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
   );
 }
 
